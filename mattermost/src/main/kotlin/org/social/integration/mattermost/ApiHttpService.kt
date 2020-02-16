b175d6aee05fc7c4b255fb96220f671b4ef6471f
Black <psgrepjava@gmail.com>
2020-02-16 19:41:51 +1100

    Extend ApiHttpService

--- mattermost/src/main/kotlin/org/social/integration/mattermost/ApiHttpService.kt
+++ mattermost/src/main/kotlin/org/social/integration/mattermost/ApiHttpService.kt
@@ -6,13 +6,23 @@ import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
 class ApiHttpService(val targetFactory:WebTargetFactory) {
-    fun post(path:String, payload:Entity<*>): Response {
-        val resp = request(path).post(payload)
+    fun <T> post(path:String, payload:Any, responsePayloadClass: Class<T>): T
+            = postEntity(path, Entity.json(payload)).readEntity(responsePayloadClass)
+
+    fun <T> postArray(path: String, array: Array<*>, responsePayloadClass: Class<T>)
+            = postEntity(path, Entity.json(array)).readEntity(responsePayloadClass)
+
+    fun postString(path:String, payload:String): String
+            = postEntity(path, Entity.text(payload)).readEntity(String::class.java)
+
+    fun postEntity(path:String, entity: Entity<*>): Response {
+        val resp = request(path).post(entity)
         if(resp.status >= 400) {
             throw WebApplicationException(resp)
         }
         return resp
     }
 
-    fun request(path:String) = targetFactory.getTarget(path).request(MediaType.APPLICATION_JSON)
+    fun request(path:String)
+            = targetFactory.getTarget(path).request(MediaType.APPLICATION_JSON)
 }

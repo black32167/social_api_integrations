ac27b0ecb0c8841855d52f092ba6422a8763224a
Black <psgrepjava@gmail.com>
2020-02-16 19:42:21 +1100

    Renaming MM services

--- mattermost/src/main/kotlin/org/social/integration/mattermost/MattermostBridgeServer.kt
+++ mattermost/src/main/kotlin/org/social/integration/mattermost/MattermostBridgeServer.kt
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import org.social.integration.mattermost.api.MattermostAuthService
-import org.social.integration.mattermost.api.MattermostMessageService
-import org.social.integration.mattermost.api.MattermostUserService
+import org.social.integration.mattermost.api.MMAuthService
+import org.social.integration.mattermost.api.MMMessageService
+import org.social.integration.mattermost.api.MMUserService
 import social.api.auth.server.AuthApiResource
 import social.api.message.server.MessageApiResource
 import social.api.server.JaxRsServer
@@ -36,9 +36,9 @@ class MattermostBridgeServer(
     val targetFactory = WebTargetFactory(mmUrl)
     val httpService = ApiHttpService(targetFactory)
     fun resources() = arrayOf<Any>(
-            MessageApiResource(MattermostMessageService(httpService)),
-            UserApiResource(MattermostUserService(httpService)),
-            AuthApiResource(MattermostAuthService(httpService)),
+            MessageApiResource(MMMessageService(httpService)),
+            UserApiResource(MMUserService(httpService)),
+            AuthApiResource(MMAuthService(httpService)),
             WebApplicationExceptionMapper()
     )
 

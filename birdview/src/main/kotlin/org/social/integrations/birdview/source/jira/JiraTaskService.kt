7c1b41c292f0696f4114209dd9b49e07fd0f01ea
Black <psgrepjava@gmail.com>
2020-04-13 13:42:38 +1000

    Retrieving 'updated' and 'http_url' for jira

--- birdview/src/main/kotlin/org/social/integrations/birdview/source/jira/JiraTaskService.kt
+++ birdview/src/main/kotlin/org/social/integrations/birdview/source/jira/JiraTaskService.kt
@@ -10,15 +10,16 @@ import social.api.task.model.Tasks
 import social.api.task.server.TaskApiService
 import javax.inject.Named
 import javax.ws.rs.client.Entity
+import javax.ws.rs.client.WebTarget
 
 @Named
 class JiraTaskService(
         jiraConfigProvider: BVJiraConfigProvider
 ): TaskApiService {
     private val jiraConfig: BVJiraConfig = jiraConfigProvider.getJira()
-    private val targetFactory: WebTargetFactory = WebTargetFactory(jiraConfig.baseUrl) {
+    private val jiraRestTarget: WebTarget = WebTargetFactory(jiraConfig.baseUrl) {
         BasicAuth(jiraConfig.user, jiraConfig.token)
-    }
+    }.getTarget("/rest/api/2")
 
     override fun getTask(p0: String?): Task {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -29,7 +30,7 @@ class JiraTaskService(
     }
 
     override fun getTasks(status: String): Tasks {
-        val jiraIssuesResponse = targetFactory.getTarget("search").request().post(Entity.json(JiraIssuesFilterRequest(
+        val jiraIssuesResponse = jiraRestTarget.path("search").request().post(Entity.json(JiraIssuesFilterRequest(
                 maxResults = 10,
                 jql = "(assignee = currentUser() or watcher = currentUser()) and status in (\"${status}\") order by lastViewed DESC"
         )))
@@ -40,7 +41,11 @@ class JiraTaskService(
 
         val jiraIssuesContainer = jiraIssuesResponse.readEntity(JiraIssuesFilterResponse::class.java)
 
-        val tasks = jiraIssuesContainer.issues.map { issue -> Task().apply { setId(issue.key) } }
+        val tasks = jiraIssuesContainer.issues.map { issue -> Task().apply {
+            id = issue.key
+            updated = issue.fields.updated
+            httpUrl = "${jiraConfig.baseUrl}/browse/${issue.key}"
+        } }
         return Tasks().apply { setTasks(tasks) }
     }
 }

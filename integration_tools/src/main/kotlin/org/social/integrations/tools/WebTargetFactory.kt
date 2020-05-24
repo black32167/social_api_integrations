fa1d1a47641130d5f31e66f5b93ea8681fa65219
Black <psgrepjava@gmail.com>
2020-05-25 09:48:52 +1000

    Bigger timeouts

--- integration_tools/src/main/kotlin/org/social/integrations/tools/WebTargetFactory.kt
+++ integration_tools/src/main/kotlin/org/social/integrations/tools/WebTargetFactory.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.glassfish.jersey.client.ClientConfig
+import org.glassfish.jersey.client.ClientProperties
 import org.social.integrations.tools.filter.ClientAuthFilter
 import social.api.server.auth.ApiAuth
 import java.net.URI
@@ -13,12 +14,17 @@ import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.client.WebTarget
 
 class WebTargetFactory(url:String, val authProvider: () -> ApiAuth? = {null}) {
+    private val timeoutMs = 1000000
     private val client = buildClient()
 
     private fun buildClient(): Client =
         ClientBuilder.newClient(
                 ClientConfig(ClientAuthFilter(authProvider))
                         .property("jersey.config.jsonFeature", "disabled")
+                        .property(ClientProperties.CONNECT_TIMEOUT, timeoutMs)
+                        .property(ClientProperties.READ_TIMEOUT, timeoutMs)
+                        .property("http.connection.timeout", timeoutMs)
+                        .property("http.receive.timeout", timeoutMs)
                         .register(JacksonJsonProvider(ObjectMapper()
                                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                                 .registerModule(KotlinModule())))

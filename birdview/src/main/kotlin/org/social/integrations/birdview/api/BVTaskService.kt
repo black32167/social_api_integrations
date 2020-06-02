4e01082d3102a994f0beee59d28e7f97ce31f48b
Black <psgrepjava@gmail.com>
2020-06-02 12:08:28 +1000

    Grouping orphaned documents

--- birdview/src/main/kotlin/org/social/integrations/birdview/api/BVTaskService.kt
+++ birdview/src/main/kotlin/org/social/integrations/birdview/api/BVTaskService.kt
@@ -11,6 +11,7 @@ import org.social.integrations.birdview.source.trello.TrelloTaskService
 import org.social.integrations.birdview.utils.BVConcurrentUtils
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import java.util.concurrent.Future
 import javax.inject.Named
 
 @Named
@@ -21,24 +22,16 @@ class BVTaskService(
         private val github: GithubTaskService
 )  {
     private val executor = Executors.newFixedThreadPool(3, BVConcurrentUtils.getDaemonThreadFactory())
-   // private val tfIdfCalclulator = TfIdfCalclulator()
 
     fun getTaskGroups(request: TasksRequest): List<BVDocument> {
-        val groupingIdsMap = mutableMapOf<BVDocumentId, List<BVDocument>>()
-        val docs = mutableListOf<BVDocument>()
-        val start = System.currentTimeMillis()
         val sources = listOf<BVTaskSource>(jira, trello, github)
 
-        val futures = sources.map { source ->
-            executor.submit(Callable<List<BVDocument>> { source.getTasks(request) })
-        }
-        for (future in futures) {
-            try {
-                docs.addAll(future.get())
-            } catch (e:Exception) {
-                e.printStackTrace()
-            }
-        }
+        val docs:MutableList<BVDocument> = sources
+                .map { source -> executor.submit(Callable<List<BVDocument>> { source.getTasks(request) }) }
+                .map { getSwallowException(it) }
+                .filterNotNull()
+                .flatten()
+                .toMutableList()
 
         linkDocs(docs)
 
@@ -50,17 +43,38 @@ class BVTaskService(
             acc
         }
 
+        val groupedDocsIds = groupedDocumentsMap.values
+                .flatten()
+                .flatMap { it.ids }
+                .toSet()
+
+        // Remove grouped documents
+        val orphanedDocs = docs.filter { doc -> doc.ids.none(groupedDocsIds::contains) }
+
         val groupedDocuments:List<BVDocument> = groupedDocumentsMap
                 .map { (groupDocId, collection) -> newGroupDoc(groupDocId, collection) }
-                .sortedByDescending { it.getLastUpdated() }
+                .sortedByDescending { it.getLastUpdated() } +
+                listOf(newGroupDoc(null, orphanedDocs).apply { title = "--- Others ---" })
+
 
         groupDescriber.describe(groupedDocuments)
 
         return groupedDocuments
     }
 
-    private fun newGroupDoc(groupDocId: BVDocumentId, collection: List<BVDocument>):BVDocument =
-            BVDocument(ids = listOf(groupDocId), subDocuments = collection.toMutableList())
+    private fun <T> getSwallowException(future: Future<T>): T? {
+        try {
+            return future.get()
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun newGroupDoc(groupDocId: BVDocumentId?, collection: List<BVDocument>):BVDocument =
+            BVDocument(
+                    ids = groupDocId?.let { listOf(it) } ?: emptyList(),
+                    subDocuments = collection.toMutableList())
 
     private fun linkDocs(docs: MutableList<BVDocument>) {
         val groupId2Group = docs
